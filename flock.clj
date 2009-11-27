d1fec213f80dc44c58593f2ad077eb924a50c814
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-11-27 10:18:42 +0100

    added rendering

--- flock.clj
+++ flock.clj
@@ -34,3 +34,38 @@
 
 (defn stop []
   (reset! running false))
+
+;;;;;;;;;;;;;;; RENDERING ;;;;;;;;;;;;;;;;;;;;;;;
+
+(def scale 0.5)
+
+(import 
+ '(java.awt Color Dimension)
+ '(java.awt.image BufferedImage)
+ '(javax.swing JPanel JFrame))
+
+(defn render-bird [g bird]
+  (let [x (* scale (:x bird))
+        y (* scale (:y bird))]
+    (doto g
+      (.setColor Color/black)
+      (.fillRect x y 5 5))))
+
+(defn render [g]
+  (let [img (BufferedImage. (* scale dim) (* scale dim) BufferedImage/TYPE_INT_RGB)
+        bg (.getGraphics img)]
+    (doto bg
+      (.setColor Color/white)
+      (.fillRect 0 0 (.getWidth img) (.getHeight img)))
+    (doseq [bird (map deref birds)]
+      (render-bird bg bird))
+    (.drawImage g img 0 0 nil)
+    (.dispose bg)))
+
+(def panel (doto (proxy [JPanel] [] (paint [g] (render g)))
+             (.setPreferredSize (Dimension. (* scale dim) (* scale dim)))))
+
+(def frame (doto (new JFrame) 
+             (.add panel)
+             .pack
+             .show))
