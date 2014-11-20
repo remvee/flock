3c0bbca43259ae9b5f6c6c7334d0137bd7d5c827
Remco van 't Veer <remco@remworks.net>
2014-11-20 15:40:55 +0100

    Add cljs version

--- project.clj
+++ project.clj
@@ -1,3 +1,14 @@
 (defproject flock "1.0.0-SNAPSHOT"
   :description "Simulate a flock of.. whatever.."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371" :scope "provided"]
+                 [reagent "0.4.2"]]
+
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  :cljsbuild {:builds {:app {:source-paths ["src"]
+                             :compiler {:preamble ["reagent/react.js"]
+                                        :output-to "resources/public/js/app.js"
+                                        :output-dir "resources/public/js/app"
+                                        :optimizations :none}}}})
---
+++ src/flock/core.cljs
@@ -0,0 +1,175 @@
+(ns flock.core
+  (:require [clojure.string :as s]
+            [reagent.core :as reagent :refer [atom]]))
+
+(def n-birds 15)
+(def max-speed (atom 15))
+(def drunkness (atom 3))
+(def leader-attraction (atom 1))
+(def privacy-radius (atom 100))
+
+(def dim {:width 1500 :height 1000})
+(def behave-sleep-ms 50)
+
+(defn create-bird []
+  {:id (rand)
+   :x (rand (:width dim))
+   :y (rand (:height dim))
+   :dx (- (rand @max-speed) (rand @max-speed))
+   :dy (- (rand @max-speed) (rand @max-speed))})
+
+(def running (atom false))
+
+(def birds-atom (atom (map (fn [_] (create-bird)) (range n-birds))))
+
+;;;;;;;;;;;;;;; HELPERS ;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn vector-length [x y]
+  "Length of a vector."
+  (Math/sqrt (+ (* x x) (* y y))))
+
+(defn bird-speed [bird]
+  (vector-length (:dx bird) (:dy bird)))
+
+(defn distance-between [this that]
+  (vector-length (- (:x this) (:x that))
+                 (- (:y this) (:y that))))
+
+(defn- neighbors-sorted-by-distance [bird birds]
+  "Return list of birds sorted by distance from given bird."
+  (sort-by #(vector-length (- (:x bird) (:x %))
+                           (- (:y bird) (:y %))) birds))
+
+(defn furthest-bird [bird birds]
+  (last (neighbors-sorted-by-distance bird birds)))
+
+(defn closest-bird [bird birds]
+  (second (neighbors-sorted-by-distance bird birds)))
+
+;;;;;;;;;;;;;;; BEHAVIOUR ;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn move [bird]
+  (assoc bird
+    :x (+ (:x bird) (:dx bird))
+    :y (+ (:y bird) (:dy bird))))
+
+(defn bounce-world [bird]
+  (let [bound (fn [v d dim]
+                (cond (> v dim)  (* -1 (Math/abs d))
+                      (neg? v)   (Math/abs d)
+                      :otherwise d))]
+    (assoc bird
+      :dx (bound (:x bird) (:dx bird) (:width dim))
+      :dy (bound (:y bird) (:dy bird) (:height dim)))))
+
+(defn bounce-others [bird birds]
+  (let [neighbor (closest-bird bird birds)
+        distance (distance-between bird neighbor)]
+    (if (< distance @privacy-radius)
+      (let [dx (- (:x neighbor) (:x bird))
+            dy (- (:y neighbor) (:y bird))]
+        (assoc bird
+          :dx (- (:dx bird) (/ dx distance))
+          :dy (- (:dy bird) (/ dy distance))))
+      bird)))
+
+(defn follow-leader [bird birds]
+  (let [leader (furthest-bird bird birds)
+        dx     (- (:x leader) (:x bird))
+        dy     (- (:y leader) (:y bird))
+        n      (max (Math/abs dx) (Math/abs dy))]
+    (assoc bird
+      :dx (+ (:dx bird) (* @leader-attraction (/ dx n)))
+      :dy (+ (:dy bird) (* @leader-attraction (/ dy n))))))
+
+(defn cap-speed [bird]
+  (let [speed (bird-speed bird)]
+    (if (> speed @max-speed)
+      (let [f (/ @max-speed speed)]
+        (assoc bird
+          :dx (* (:dx bird) f)
+          :dy (* (:dy bird) f)))
+      bird)))
+
+(defn stumble [bird]
+  (assoc bird
+    :dx (+ (:dx bird) (- (/ @drunkness 2) (rand @drunkness)))
+    :dy (+ (:dy bird) (- (/ @drunkness 2) (rand @drunkness)))))
+
+(defn behave [bird birds]
+  (-> bird
+      (follow-leader birds)
+      (bounce-others birds)
+      stumble
+      cap-speed
+      bounce-world
+      move))
+
+;;;;;;;;;;;;;;; LIFE ;;;;;;;;;;;;;;;
+
+(defn run []
+  (when @running (js/setTimeout run behave-sleep-ms))
+  (swap! birds-atom (fn [birds] (map #(behave % birds) birds))))
+
+(defn start []
+  (when-not @running
+    (reset! running true)
+    (run)))
+
+(defn stop []
+  (reset! running false))
+
+;;;;;;;;;;;;;;; RENDERING ;;;;;;;;;;;;;;;
+
+(defn bird-component [bird]
+  (let [{:keys [id x y dx dy]} bird]
+    [[:rect {:key (str "head-" id)
+             :x (- x 6)
+             :y (- y 6)
+             :rx 12
+             :ry 12
+             :width 12
+             :height 12
+             :stroke "white"
+             }]
+     [:line {:key (str "tail-" id),
+             :x1 x, :y1 y,
+             :x2 (- x (* dx 2)), :y2 (- y (* dy 2)),
+             :stroke "black", :stroke-width 4}]]))
+
+(defn birds-component []
+  [:g (mapcat bird-component @birds-atom)])
+
+(defn input-range [name atom attrs]
+  [:label
+   [:span (str " " name " (" @atom ")")]
+   [:input (merge {:type "range"
+                   :value @atom
+                   :on-change #(reset! atom
+                                       (js/parseFloat (.-value (.-target %))))}
+                  attrs)]])
+
+(defn main-component []
+  [:div
+   [:svg {:xmlns "http://www.w3.org/2000/svg"
+          :version "1.1"
+          :viewBox (s/join " " [0 0 (:width dim) (:height dim)])}
+    [birds-component]]
+   [:div.controles
+    (input-range "Max. speed" max-speed
+                 {:min 0 :max 50})
+    (input-range "Leader attraction" leader-attraction
+                 {:min 0 :max 5 :step 0.1})
+    (input-range "Drunkness" drunkness
+                 {:min 0 :max 25})
+    (input-range "Privacy radius" privacy-radius
+                 {:min 0 :max 500})
+    [:div.stop-start
+     (if @running
+       [:button {:on-click stop} "stop"]
+       [:button {:on-click start} "start"])]]])
+
+(defn ^:export main []
+  (let [el (.getElementById js/document "app")]
+    (reagent/render-component [main-component] el)
+    (start)))
